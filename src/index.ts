eb7bf6125580b09987df6504af1de182415fffb2
robotsix-mill <mill@robotsix.local>
2026-08-01 12:49:19 +0000

    mill: Establish robotsix-ui as the common styling base for fleet board/dashboard UIs (20260801T123137Z-establish-robotsix-ui-as-the-common-styl-5c5b)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import "./styles/index.css";
+
 export { ConfigPanel } from "./components/ConfigPanel/index.js";
 export type { ConfigPanelProps } from "./components/ConfigPanel/index.js";
 export { FormField } from "./components/ConfigPanel/FormField.js";
